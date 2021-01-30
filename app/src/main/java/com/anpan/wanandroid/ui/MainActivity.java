49f915e35117a28c59a8f7d694da5c46030ca783
liubo <liubo@netinfosz.com>
2021-01-30 21:26:14 +0800

    创建项目

--- app/src/main/java/com/anpan/wanandroid/ui/MainActivity.java
+++ app/src/main/java/com/anpan/wanandroid/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.anpan.wanandroid;
+package com.anpan.wanandroid.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.anpan.wanandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
